bc73b977a6d3b85e8a21ce5b2de9f12037f8ee88
Saksham1970 <45041294+Saksham1970@users.noreply.github.com>
2022-10-13 09:30:53 +0530

    Update 1.5_linear_search_binary_search.c

--- DS/1.5_linear_search_binary_search.c
+++ DS/1.5_linear_search_binary_search.c
@@ -1,92 +1,136 @@
 // Header files
 #include <stdio.h>
-
-int list[10] = {1, 4, 5, 10, 30, 15, 2, -1, -15, 20}; // Array which is used for searching
-int size = 10; // Array's size
+#include <stdlib.h>
 
 // Prototyping the functions
-int* bubble_sort(int*,int);
-void linear_search(int[],int,int);
-void binary_search(int[],int,int);
+int *bubble_sort(int *, int, int **);
+int linear_search(int[], int, int);
+int binary_search(int[], int, int);
+int *input_list(int *);
 
 int main()
 {
+    // Getting the list inputted
+    int size, *list;
+    list = input_list(&size);
+
     // Getting the element to be searched from the user
-    int search_element; 
+    int search_element;
     printf("Input the search element: ");
-    scanf("%d",&search_element);
+    scanf("%d", &search_element);
 
     // Doing searches using functions
-    linear_search(list,size,search_element);
-    binary_search(list,size,search_element);
+    int linear_index = linear_search(list, size, search_element);
+    int binary_index = binary_search(list, size, search_element);
+
+    if (linear_index >= 0)
+        printf("Found the element using linear search at index %d.\n", linear_index);
+    else
+        printf("Could not find the element using linear search.\n", linear_index);
+
+    if (binary_index >= 0)
+        printf("Found the element using binary search at index %d. \n", binary_index);
+    else
+        printf("Could not find the element using binary search. \n", binary_index);
 
     return 0;
 }
 
-void linear_search(int list[], int size, int search_element)
-{   
-    //This function uses linear search to find the element in list
+int linear_search(int list[], int size, int search_element)
+{
+    // This function uses linear search to find the element in list
 
-    for (int i = 0; i < size; i++) //Traversing through the list
+    for (int i = 0; i < size; i++) // Traversing through the list
     {
-        if (list[i] == search_element) //If element found
-        {
-
-            printf("Found the element %d at position %d using linear search.\n", search_element, i);
-            return;
-        }
+        if (list[i] == search_element) // If element found
+            return i;
     }
 
     // If foor loop is Completed and element not found
-    printf("Could not find the element %d in the list using linear search.\n", search_element);
-    return;
+    return -1;
 }
 
-void binary_search(int list[], int size, int search_element)
+int binary_search(int list[], int size, int search_element)
 {
     // This function searches for an element using binary search
+    int *index_list;
+    int *sorted_list = bubble_sort(list, size, &index_list); // binary search uses a sorted list
 
-    int* sorted_list = bubble_sort(list,size); // binary search uses a sorted list
-    
-    int lo = 0, hi= size-1; // first and last index
+    int lo = 0, hi = size - 1; // first and last index
     int mid;
-    
+
     // while indexes not coinciding or consecutive
-    while (hi - lo > 1) {
-        int mid = (hi + lo) / 2; 
-        if (list[mid] < search_element) { //element is in the second half of the list
+    while (hi - lo > 1)
+    {
+        int mid = (hi + lo) / 2;
+        if (list[mid] < search_element)
+        { // element is in the second half of the list
             lo = mid + 1;
         }
-        else {                             //element is in the first half of the list
+        else
+        { // element is in the first half of the list
             hi = mid;
         }
     }
+    int lo_actual_index = index_list[lo];
+    int hi_actual_index = index_list[hi];
+
+    free(index_list);
 
     // Loop finished with finding the element
-    if (list[lo] == search_element || list[hi] == search_element) 
-        printf("Found the element %d in the list using binary search.", search_element);    
+    if (list[lo] == search_element)
+        return lo_actual_index;
+
+    if (list[hi] == search_element)
+        return hi_actual_index;
 
     // Loop finished without finding the element
     else
-        printf("Could not find the element %d in the list using binary search.\n", search_element);
-    
+        return -1;
 }
 
-int *bubble_sort(int list[], int size)
+int *bubble_sort(int list[], int size, int **index_list)
 {
-    //This function sorts the list using bubble sort
+    // This function sorts the list using bubble sort
+    int *local_index_list = (int *)malloc(size * sizeof(int));
+
+    for (int i = 0; i < size; i++)
+    {
+        local_index_list[i] = i;
+    }
+
+    *index_list = local_index_list;
 
     for (int i = 0; i < size - 1; i++)
     {
         for (int j = 0; j < size - i - 1; j++)
         {
-            if (list[j] > list[j+1]) 
+            if (list[j] > list[j + 1])
             {
                 int swap_var = list[j];
                 list[j] = list[j + 1];
                 list[j + 1] = swap_var;
+
+                swap_var = local_index_list[j];
+                local_index_list[j] = local_index_list[j + 1];
+                local_index_list[j + 1] = swap_var;
             }
         }
     }
     return list;
+}
+
+int *input_list(int *size)
+{
+    int *array;
+    printf("Input the number of elements in the list: ");
+    scanf("%d", size);
+
+    array = (int *)malloc(*size * sizeof(int));
+    for (int i = 0; i < *size; i++)
+    {
+        printf("Input element no %d of the list: ", i + 1);
+        scanf("%d", array + i);
+    }
+    return array;
 }
