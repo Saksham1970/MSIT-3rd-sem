7d67dcfd204d3ec708a84c6270be8b5fb1a59f15
Saksham1970 <45041294+Saksham1970@users.noreply.github.com>
2022-12-26 09:08:57 +0530

    Update 13_runge_kutta.cpp

--- CM/13_runge_kutta.cpp
+++ CM/13_runge_kutta.cpp
@@ -1,31 +1,47 @@
-#include<iostream>
+#include<iostream> // header files
 
 float differential_eq(float x, float y){
+    // The equation on which runge kutta to be applied
     return (y * y - x * x) / (y * y + x * x);
 }
 
+//Function Prototyping
 float runge_kutta(float x0, float y0, float h);
 
+//Driver code
 int main()
 {
     float h, x0, y0;
-
-    std::cout << "\nEnter x0: ";
+    int n;
+    
+    std::cout << "Differential equation: (y^2 - x^2)/(y^2 + x^2) \n";
+    
+    std::cout << "Enter x0: ";
     std::cin >> x0;
 
-    std::cout << "\nEnter y0: ";
+    std::cout << "Enter y0: ";
     std::cin >> y0;
 
-    std::cout << "\nEnter h: ";
+    std::cout << "Enter h: ";
     std::cin >> h;
-
-    std::cout << "Y with differential equation (y^2 - x^2)/(y^2 + x^2) at X = "<< x0 + h << " is: " << runge_kutta(x0, y0, h);
+    
+    std::cout << "Enter n: ";
+    std::cin >> n;
+    
+    //Loop to apply runge kutta method again and again for n iterations
+    for(int i =1; i <= n; i++)
+    {
+        y0 = runge_kutta(x0, y0, h);
+        x0 += h;
+        std::cout<<"X"<<i<<"= "<<x0<<" Y"<<i<<"= "<<y0<<std::endl;
+    }
+    
 
     return 0;
 }
 
 float runge_kutta(float x0, float y0, float h){
-    
+    // This function applies runge kutta method
     float k1, k2, k3, k4;
     k1 = h * differential_eq(x0, y0);
     k2 = h * differential_eq(x0 + h / 2, y0 + k1 / 2);
